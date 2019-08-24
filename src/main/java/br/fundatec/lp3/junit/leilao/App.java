3bb94397e59b475bf08f737bb072441e979b65a2
vinicius.trindade <vinicius.trindade@zenvia.com>
2019-08-24 15:09:51 -0300

    Evolucao do codigo e testes unitarios

--- src/main/java/br/fundatec/lp3/junit/leilao/App.java
+++ src/main/java/br/fundatec/lp3/junit/leilao/App.java
@@ -2,7 +2,7 @@ package br.fundatec.lp3.junit.leilao;
 
 public class App {
 	
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
        
     	Usuario joao = new Usuario("Joao");
     	Usuario maria = new Usuario("Maria");
@@ -14,16 +14,30 @@ public class App {
     	Lance lanceDaMaria = new Lance(maria, 400.0);
     	Lance lanceDoJose = new Lance(jose, 250.0);
     	
+    	leilao.propoe(lanceDoJose);
     	leilao.propoe(lanceDoJoao);
     	leilao.propoe(lanceDaMaria);
-    	leilao.propoe(lanceDoJose);
     	
     	Avaliador avaliador = new Avaliador();
     	avaliador.avalia(leilao);
     	
-    	System.out.println(leilao.toString());        	
-    	System.out.println("Maior lance: ");
-    	System.out.println(avaliador.getMaiorLance().toString());
+    	double menorValorEsperado = 250.0;
+    	double maiorValorEsperado = 400.0;
+    	
+    	double valorMenorLance = avaliador.getMenorLance().getValor();
+    	double valorMaiorLance = avaliador.getMaiorLance().getValor();
+    	
+    	
+    	boolean valorMaiorEstaCorreto = (valorMaiorLance == maiorValorEsperado);
+    	boolean valorMenorEstaCorreto = (valorMenorLance == menorValorEsperado);
+    	
+    	System.out.println(leilao.toString());   
+    	System.out.println("Retorna maior lance correto: " + valorMaiorEstaCorreto);
+    	System.out.println("Retorna menor lance correto: " + valorMenorEstaCorreto);
+//    	System.out.println("Maior lance: ");
+//    	System.out.println(avaliador.getMaiorLance().toString());
+//    	System.out.println("Menor lance: ");
+//    	System.out.println(avaliador.getMenorLance().toString());
 
     }
 }
--- src/main/java/br/fundatec/lp3/junit/leilao/Avaliador.java
+++ src/main/java/br/fundatec/lp3/junit/leilao/Avaliador.java
@@ -4,23 +4,36 @@ public class Avaliador {
 	
 	//seta o valor inicial da variavel com o menor valor possível
 	private Lance maiorLance;
+	private Lance menorLance;
 	
 	public Avaliador() {
 		maiorLance = new Lance(new Usuario(""), Double.NEGATIVE_INFINITY);
+		menorLance = new Lance(new Usuario(""), Double.POSITIVE_INFINITY);
 	}
 	
-	public void avalia(Leilao leilao) {
+	public void avalia(Leilao leilao) throws Exception {
+		
+		if(leilao.getLances().isEmpty()) {
+			throw new Exception("Leilao deve conter pelo menos um lance");
+		}
 		
 		for(Lance lance : leilao.getLances()) {
 			
 			if(lance.getValor() > maiorLance.getValor()) {
 				maiorLance = lance;
 			}
+			if (lance.getValor() < menorLance.getValor()) {
+				menorLance = lance;
+			}
 		}
 	}
 	
 	public Lance getMaiorLance() {
 		return maiorLance;
+	}
+	
+	public Lance getMenorLance() {
+		return menorLance;
 	}
 
 }
