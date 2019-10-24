36020fe6739f7c6b88b063667960a02985fee485
vinicius <viniciusvks@gmail.com>
2019-10-24 00:26:14 -0300

    Testes unitarios com mocks

--- src/test/java/br/fundatec/lp3/junit/encerrador/EncerradorDeLeilaoTest.java
+++ src/test/java/br/fundatec/lp3/junit/encerrador/EncerradorDeLeilaoTest.java
@@ -1,89 +1,132 @@
 package br.fundatec.lp3.junit.encerrador;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.mockito.Mockito;
 
 import br.fundatec.lp3.junit.dao.LeilaoDao;
-import br.fundatec.lp3.junit.dao.UsuarioDao;
 import br.fundatec.lp3.junit.leilao.Lance;
 import br.fundatec.lp3.junit.leilao.Leilao;
 import br.fundatec.lp3.junit.leilao.Usuario;
 
 public class EncerradorDeLeilaoTest {
 
-	private static ClassPathXmlApplicationContext ctx;
-	private static UsuarioDao tabelaUsuarios;
-	private static LeilaoDao tabelaLeiloes;
-
-	@BeforeClass
-	public static void initAppContext() {
-
-		ctx = new ClassPathXmlApplicationContext("beans.xml");
-		tabelaUsuarios = (UsuarioDao) ctx.getBean("usuarioDao");
-		tabelaLeiloes = (LeilaoDao) ctx.getBean("leilaoDao");
-
-	}
-
-	@AfterClass
-	public static void closeAppContext() {
-
-		ctx.close();
-
-	}
-
-	@Before
-	public void setUp() {
-
-		tabelaUsuarios.limpa();
-
-		tabelaUsuarios.cria(new Usuario("Joao"));
-		tabelaUsuarios.cria(new Usuario("Maria"));
-		tabelaUsuarios.cria(new Usuario("Jose"));
-
-	}
-
-	@After
-	public void tearDown() {
-
-		tabelaUsuarios.limpa();
-		tabelaLeiloes.limpa();
-
-	}
-
 	@Test
 	public void testaEncerramentoParaMultiplosLeiloes() throws Exception {
- 
-		
+
+		Usuario joao = new Usuario("Joao");
+		Lance lanceDoJoao = new Lance(joao, 10.0);
+		Leilao leilaoVideoGame = new Leilao("Video Game");
+		leilaoVideoGame.setData(diasAtras(10));
+		leilaoVideoGame.propoe(lanceDoJoao);
+
+		Usuario maria = new Usuario("Maria");
+		Lance lanceDaMaria = new Lance(maria, 20.0);
+		Leilao leilaoComputador = new Leilao("Computador");
+		leilaoComputador.setData(diasAtras(10));
+		leilaoComputador.propoe(lanceDaMaria);
+
+		List<Leilao> listaDeLeiloes = new ArrayList<Leilao>();
+		listaDeLeiloes.add(leilaoVideoGame);
+		listaDeLeiloes.add(leilaoComputador);
+
+		LeilaoDao dao = Mockito.mock(LeilaoDao.class);
+		Mockito.when(dao.ativos()).thenReturn(listaDeLeiloes);
+
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao);
+		encerrador.encerraLeiloesAntigos();
+
+		int numeroEsperadoDeLeiloesEncerrados = 2;
+
+		assertTrue(leilaoVideoGame.estaEncerrado());
+		assertTrue(leilaoComputador.estaEncerrado());
+		assertEquals(numeroEsperadoDeLeiloesEncerrados, encerrador.getTotalEncerrados());
+
 	}
 
 	@Test
 	public void testaEncerramentoParaListaVaziaDeLeiloes() throws Exception {
-		
+
+		LeilaoDao dao = Mockito.mock(LeilaoDao.class);
+		Mockito.when(dao.ativos()).thenReturn(new ArrayList<Leilao>());
+
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao);
+		encerrador.encerraLeiloesAntigos();
+
+		int numeroEsperadoDeLeiloesEncerrados = 0;
+
+		assertEquals(numeroEsperadoDeLeiloesEncerrados, encerrador.getTotalEncerrados());
 
 	}
 
 	@Test
 	public void testaEncerramentoParaLeiloesPrestesASeremEncerrados() throws Exception {
 
-		
+		Usuario joao = new Usuario("Joao");
+		Lance lanceDoJoao = new Lance(joao, 10.0);
+		Leilao leilaoVideoGame = new Leilao("Video Game");
+		leilaoVideoGame.setData(diasAtras(6));
+		leilaoVideoGame.propoe(lanceDoJoao);
+
+		Usuario maria = new Usuario("Maria");
+		Lance lanceDaMaria = new Lance(maria, 20.0);
+		Leilao leilaoComputador = new Leilao("Computador");
+		leilaoComputador.setData(diasAtras(7));
+		leilaoComputador.propoe(lanceDaMaria);
+
+		List<Leilao> listaDeLeiloes = new ArrayList<Leilao>();
+		listaDeLeiloes.add(leilaoVideoGame);
+		listaDeLeiloes.add(leilaoComputador);
+
+		LeilaoDao dao = Mockito.mock(LeilaoDao.class);
+		Mockito.when(dao.ativos()).thenReturn(listaDeLeiloes);
+
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao);
+		encerrador.encerraLeiloesAntigos();
+
+		int numeroEsperadoDeLeiloesEncerrados = 1;
+
+		assertFalse(leilaoVideoGame.estaEncerrado());
+		assertTrue(leilaoComputador.estaEncerrado());
+		assertEquals(numeroEsperadoDeLeiloesEncerrados, encerrador.getTotalEncerrados());
+
 	}
 
-	@Test
-	public void testaEncerramentoParaLeilaoSemLances() {
- 
+	@Test(expected=Exception.class)
+	public void testaEncerramentoParaLeilaoSemLances() throws Exception {
+
+		Leilao leilaoVideoGame = new Leilao("Video Game");
+		leilaoVideoGame.setData(diasAtras(6));
+
+		Leilao leilaoComputador = new Leilao("Computador");
+		leilaoComputador.setData(diasAtras(7));
+
+		List<Leilao> listaDeLeiloes = new ArrayList<Leilao>();
+		listaDeLeiloes.add(leilaoVideoGame);
+		listaDeLeiloes.add(leilaoComputador);
+
+		LeilaoDao dao = Mockito.mock(LeilaoDao.class);
+		Mockito.when(dao.ativos()).thenReturn(listaDeLeiloes);
+
+		try {
+
+			EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao);
+			encerrador.encerraLeiloesAntigos();
+
+		} catch (Exception e) {
+			assertEquals("Leilao deve ter pelo menos um lance para ser encerrado", e.getMessage());
+			throw e;
+		}
 
 	}
-	
+
 	private LocalDate diasAtras(int dias) {
 		return LocalDate.now().minusDays(dias);
 	}
